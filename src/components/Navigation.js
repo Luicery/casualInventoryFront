d8b31e5865407c22833035ef7416ca08210ec006
Luicery <56919859+Luicery@users.noreply.github.com>
2021-12-13 02:53:08 +1300

    Back to animeJS, library has problem a on react but lots of trial and error to receive satisfactory first result

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,36 @@
 import React, {useState, useEffect} from "react"
 import styles from "../css/Navigation.module.css"
+import hamborger from "../assets/hamborger.png"
+import Anime from "react-anime"
 function Navigation(props) {
-  const [scroll, setScroll] = useState("")
+  const [menuShow, setMenuShow] = useState(false);
   const logout = () => {
-    localStorage.clear("jwt")
-  }
-  useEffect(() => {
-    window.addEventListener("scroll", () => {
-      setScroll(window.scrollY > window.innerHeight-38)
-    })
-  }, [])
+    localStorage.clear("jwt");
+  };
+  function menuShowFunc(event) {
+    setMenuShow(true);
+    document.getElementsByClassName("navigationOpacity")[0].style.opacity="0.5";
+  };
+  function menuCloseFunc(event) {
+    setMenuShow(false);
+    document.getElementsByClassName("navigationOpacity")[0].style.opacity="1";
+  };
   return(
     <div>
-      {window.location.href === "http://localhost:3001/" && <nav id={scroll ? styles.navigationBar : styles.navigationBarHome}>
-        <a id="homeLink" href="/">Home</a>
-        {localStorage.jwt === undefined && <a id="loginLink" class={styles.floatRight} href="/login">Login</a>}
-        {localStorage.jwt === undefined && <a id="signupLink" class={styles.floatRight} href="/signup">Signup</a>}
-        {localStorage.jwt !== undefined && <a id="logoutLink" class={styles.floatRight} href="/" onClick={logout}>Logout</a>}
-      </nav>}
-      {window.location.href !== "http://localhost:3001/" && <nav id={styles.navigationBar}>
-        <a id="homeLink" href="/">Home</a>
-        {localStorage.jwt === undefined && <a id="loginLink" class={styles.floatRight} href="/login">Login</a>}
-        {localStorage.jwt === undefined && <a id="signupLink" class={styles.floatRight} href="/signup">Signup</a>}
-        {localStorage.jwt !== undefined && <a id="logoutLink" class={styles.floatRight} href="/" onClick={logout}>Logout</a>}
-      </nav>}
+      <nav class={menuShow ? "" : styles.navigationBarFull} id={styles.navigationBar}>
+        {menuShow !== true && <img id={styles.hamborger} src={hamborger} width="40px" height="40px" onClick={menuShowFunc}></img>}
+
+        {menuShow === true && <div id={styles.menu}>
+        <Anime>
+          <span id={styles.menuName}>Supply Chain Solutions</span>
+          <span id={styles.menuClose} class={styles.floatRight} onClick={menuCloseFunc}>X</span>
+          <a id="homeLink" class={styles.menuItem} href="/">Home</a>
+          {localStorage.jwt === undefined && <a id="loginLink" class={styles.menuItem}  href="/login">Login</a>}
+          {localStorage.jwt === undefined && <a id="signupLink" class={styles.menuItem} href="/signup">Signup</a>}
+          {localStorage.jwt !== undefined && <a id="logoutLink" class={styles.menuItem} href="/" onClick={logout}>Logout</a>}
+        </Anime>
+        </div>}
+      </nav>
     </div>
   )
 }
